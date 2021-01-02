77848121871a564cba2514745368445f34b7f377
wakaJP <75990824+wakaJP@users.noreply.github.com>
2021-01-02 20:55:47 +0900

    1-3課題提出
    1-3課題作成

---
+++ JavaStudy/example2.java
@@ -0,0 +1,10 @@
+public class example2 {
+
+  public static void main(String[] args) { 
+
+//int型を宣言して、「seisu」という変数名をつけて、1234で初期化する。
+    int seisu = 1234;
+    System.out.println("int型 = 整数 = " + seisu);
+
+  } 
+}
