e23c00e1baeacbb64dedc0e6a4fc7304ba8a2433
wakaJP <75990824+wakaJP@users.noreply.github.com>
2021-01-02 20:41:53 +0900

    課題1-1,1-2
    1-1,1-2課題作成

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,49 @@
+/**
+ * Task1-2 : 課題内容
+ *
+ * 本課題では、配列の使い方を学んでいきましょう。
+ * 問①から問⑥まであります。
+ * 指定された値と変数名を守って記述してください。
+ *
+ * 問⑤,⑥については、コメントを入れてください。
+ *
+ */
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+        String[] country = new String[3];
+        country[0] = "JAPAN";
+        country[1] = "AMERICA";
+        country[2] = "KOREA";
+
+        // ② ①で作成した配列の要素数を出力してください。
+        System.out.println(country.length);
+
+        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+         *   1番目（先頭）の要素に 「りんご」 を代入
+         *   2番目の要素に 「もも」 を代入
+         *   3番目の要素に 「ぶどう」 を代入
+         */
+        String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
+
+
+        // ④ ③で作成した配列の2番目の要素を出力しなさい。
+
+        System.out.println(strArray[1]);
+
+        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+         *  int型の配列intArrayを宣言し、順番に10,20,30,40,50を要素として格納。
+         */
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        // ⑥下記の処理について、何をしているのかコメントを記入してください。
+        // intArrayの2番目と5番目の要素(20と50)の和を表示。 
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
